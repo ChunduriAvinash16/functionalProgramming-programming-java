805101abc267fbff214bc1a15a079e7904f1b42e
chunduriavinash <chunduriavinash6@gmail.com>
2025-07-27 20:34:14 +0530

    Custom Class Student and Terminal Operations allMatch,anyMatch and noneMatch

---
+++ src/custom/Student.java
@@ -0,0 +1,46 @@
+package custom;
+
+public class Student {
+    String name;
+    int age;
+    int marks;
+
+    public Student(String name, int age, int marks) {
+        this.name = name;
+        this.age = age;
+        this.marks = marks;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", marks=" + marks +
+                '}';
+    }
+}
