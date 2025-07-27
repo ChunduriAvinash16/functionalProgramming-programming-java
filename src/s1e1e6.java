d04c1088967e57ac5292765972afa15537c08ae6
chunduriavinash <chunduriavinash6@gmail.com>
2025-07-27 16:19:11 +0530

    exercise 5 and 6

--- src/s1e1e6.java
+++ src/s1e1e6.java
@@ -1,6 +1,6 @@
 import java.util.List;
 
-public class s1e1e4 {
+public class s1e1e6 {
     public static void main(String[] args) {
         //Print only odd numbers
         List<Integer> list = List.of(12,3,4,5,123,45,67);
@@ -19,5 +19,17 @@ public class s1e1e4 {
         //Print all the names with atleast 4 characters
         System.out.println("-------------------");
         names.stream().filter(name -> name.length() >=4).forEach(System.out::println);
+
+        //Print cubes of odd numbers
+        System.out.println("-------------------");
+        list.stream()
+                .filter(number ->number%2!=0)
+                .map(number -> number*number*number)
+                .forEach(System.out::println);
+
+        //Print the number of characters in each name
+        System.out.println("-------------------");
+        names.stream().map(String::length)
+                .forEach(System.out::println);
     }
 }
