e926cfff71223f7dba12b711baf806af8cc302c0
chunduriavinash <chunduriavinash6@gmail.com>
2025-07-27 19:31:09 +0530

    MethodReferncesExample for static methods, Object methods and creating new instance

---
+++ src/MethodReferncesExample.java
@@ -0,0 +1,20 @@
+import java.util.List;
+import java.util.function.Supplier;
+
+public class MethodReferncesExample {
+    static void printName(String name) {
+        System.out.println(name);
+    }
+    @SuppressWarnings("unused")
+    public static void main(String[] args) {
+        List<String> companies = List.of("Oracle", "Amazon", "Apple", "Google");
+        companies.stream().map(str -> str.toUpperCase())
+                .forEach(str -> System.out.println(str));
+
+        companies.stream().map(String::toUpperCase)
+                .forEach(MethodReferncesExample::print);
+
+        Supplier<String> integerSupplier = () -> new String();
+        Supplier<String> integerSupplier1 = String::new;
+    }
+}
